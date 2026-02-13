97647e9e64cf28efe0073198a1de1270fe7e0d87
shivani sheregar <shivanisheregar02@gmail.com>
2026-02-13 21:09:01 +0530

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,14 +1,20 @@
-def login(username, password):
-    if username == "admin" and password == "admin123":
-        return True
-    return False
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == "admin" and password == "1234":
+            return "Access Granted 🎉"
+        else:
+            return "Invalid Credentials ❌"
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
-    u = input("Username: ")
-    p = input("Password: ")
+    app.run(debug=True)
 
-    if login(u, p):
-        print("Login successful")
-    else:
-        print("Login failed")
