4ddf7493fab9a1ab7d5158c4fe0567fbd554dda9
shivani sheregar <shivanisheregar02@gmail.com>
2026-02-26 20:27:54 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+        username = request.form.get("username")
+        password = request.form.get("password")
 
         if username == "admin" and password == "1234":
-            return "Access Granted 🎉"
+            return redirect(url_for("dashboard"))
         else:
-            return "Invalid Credentials ❌"
+            return render_template("login.html", error="Invalid Credentials ❌")
 
     return render_template("login.html")
 
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
 
