abe7f7cc4c894f9db79b53440c0316a689192009
shivani sheregar <shivanisheregar02@gmail.com>
2026-02-26 20:44:32 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,17 +1,31 @@
+
 from flask import Flask, render_template, request, redirect, url_for
+from store_data import store_pattern
+from compare_behavior import compare_pattern
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def login():
+
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
 
+        # Dummy typing pattern for demo (later replace with keystroke module)
+        new_pattern = [0.21, 0.19, 0.32, 0.28]
+
         if username == "admin" and password == "1234":
+
+            suspicious = compare_pattern(username, new_pattern)
+
+            if suspicious:
+                return "⚠ Suspicious behaviour detected"
+
+            store_pattern(username, new_pattern)
             return redirect(url_for("dashboard"))
-        else:
-            return render_template("login.html", error="Invalid Credentials ❌")
+
+        return render_template("login.html", error="Invalid Credentials ❌")
 
     return render_template("login.html")
 
